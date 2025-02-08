ede4af51bd9b9c6956c8f9f63758351dcf82f2f9
Michael Martinez <micsmar@umich.edu>
2025-02-08 00:50:12 -0500

    deleted js document

--- main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.mode-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    const mode = document.body.classList.contains('dark-mode') ? '🌙' : '☀️';
-    document.querySelector('.mode-toggle').textContent = mode;
-});
